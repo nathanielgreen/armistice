06d1817012becc70e755da3d28b113dec71c6626
Nathaniel Green <ngreen2303@gmail.com>
2016-11-04 16:21:28 +0000

    About and Contacts sections now overlapping correctly and scripts added for both

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,14 +2,28 @@ console.log("script loaded");
 
 var contactButton = document.querySelector("#contact-toggle");
 var contactDiv    = document.querySelector("#contact");
+var aboutButton   = document.querySelector("#about-toggle");
+var aboutDiv      = document.querySelector("#about");
+var video         = document.querySelector("video");
 
 contactButton.addEventListener("change", function() {
   if(contactButton.checked) {
-    console.log("test");
     contactDiv.style.opacity = 1;
+    video.style.opacity = 0.5;
   } 
   else {
-    console.log("tes2");
     contactDiv.style.opacity = 0;
+    video.style.opacity = 1;
+  };
+});
+
+aboutButton.addEventListener("change", function() {
+  if(aboutButton.checked) {
+    aboutDiv.style.opacity = 1;
+    video.style.opacity = 0.5;
+  } 
+  else {
+    aboutDiv.style.opacity = 0;
+    video.style.opacity = 1;
   };
 });
